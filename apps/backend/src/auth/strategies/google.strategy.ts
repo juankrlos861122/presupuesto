fea35a07ceda766098c18c5c3d1cd5ce13dbd205
Juan Carlos Martínez L. <soft.matloz@gmail.com>
2026-05-03 14:55:21 -0500

    google

--- apps/backend/src/auth/strategies/google.strategy.ts
+++ apps/backend/src/auth/strategies/google.strategy.ts
@@ -1,17 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth2';
-import { AuthService } from '../auth.service';
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, VerifyCallback } from "passport-google-oauth2";
+import { AuthService } from "../auth.service";
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
   constructor(private authService: AuthService) {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID || '676999413369-srbf0l03q8l1fm77fo6nap5ef0hi7n5s.apps.googleusercontent.com',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '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',
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3001/auth/google/callback',
-      scope: ['profile', 'email'],
-      authorizationURL: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=select_account',
+      clientID:
+        process.env.GOOGLE_CLIENT_ID ||
+        "676999413369-srbf0l03q8l1fm77fo6nap5ef0hi7n5s.apps.googleusercontent.com",
+      clientSecret:
+        process.env.GOOGLE_CLIENT_SECRET || "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",
+      callbackURL:
+        process.env.GOOGLE_CALLBACK_URL ||
+        "https://presupuesto-backend-39io.onrender.com/auth/google/callback",
+      scope: ["profile", "email"],
+      authorizationURL:
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=select_account",
     });
   }
 
@@ -22,11 +28,11 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     done: VerifyCallback,
   ): Promise<any> {
     const { id, name, emails, photos } = profile;
-    
+
     const user = {
       googleId: id,
       email: emails[0].value,
-      name: name.givenName + (name.familyName ? ' ' + name.familyName : ''),
+      name: name.givenName + (name.familyName ? " " + name.familyName : ""),
       picture: photos[0].value,
     };
 
